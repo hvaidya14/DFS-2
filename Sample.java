2c065c94f18644c4dbfc5592b6ad4aacb816998e
Himani.Vaidya <85447801+hvaidya14@users.noreply.github.com>
2025-01-31 18:22:11 -0800

    Update-1 Sample.java

--- Sample.java
+++ Sample.java
@@ -43,3 +43,41 @@ class Solution {
         return count;
     }
 }
+
+
+//TC :O(m*n)
+//sc : O(m*n)
+//DFS 
+class Solution {
+    int count =0;
+    public int numIslands(char[][] grid) {
+        if (grid == null) {
+            return 0;
+        }
+        int m = grid.length;
+        int n = grid[0].length;
+        int[][] dirs= {{-1, 0}, {1, 0}, {0,-1}, {0, 1}};
+        for (int i=0;i<m ;i++) {
+            for (int j=0;j<n;j++) {
+                if (grid[i][j] == '1') {
+                    count++;
+                    grid[i][j] = '2';
+                    dfs(i, j, dirs, m, n, grid);
+                }
+            }
+        }
+        return count;
+
+    }
+    private void dfs(int i, int j, int[][] dirs, int m, int n, char[][] grid) {
+        for (int[] dir:dirs) {
+            int nr = i + dir[0];
+            int nc = j + dir[1];
+            if (nr < m && nr >=0 && nc < n && nc>=0 && grid[nr][nc] =='1') {
+                grid[nr][nc] = '2';
+                dfs(nr,nc,dirs, m, n, grid);
+            }
+        }
+        return;
+    }
+}
